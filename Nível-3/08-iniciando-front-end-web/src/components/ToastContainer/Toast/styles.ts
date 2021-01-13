f0f2159b18ae7f8e88ec9ee6f33b41895ecdd880
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-01-12 23:29:51 -0300

    N3A8 - Adicionando e removendo Toasts

--- Nível-3/08-iniciando-front-end-web/src/components/ToastContainer/Toast/styles.ts
+++ Nível-3/08-iniciando-front-end-web/src/components/ToastContainer/Toast/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-interface ToastProps {
+interface ContainerProps {
   type?: 'success' | 'error' | 'info';
   hasDescription: boolean;
 }
@@ -20,15 +20,7 @@ const toastTypeVariations = {
   `
 };
 
-export const Container = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 30px;
-  overflow: hidden;
-`;
-
-export const Toast = styled.div<ToastProps>`
+export const Container = styled.div<ContainerProps>`
   width: 360px;
   position: relative;
   padding: 16px 30px 16px 16px;
