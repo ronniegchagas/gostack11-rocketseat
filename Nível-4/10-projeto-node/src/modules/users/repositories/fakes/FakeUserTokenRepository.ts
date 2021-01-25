0eb66ac4cae17df3a95465a89b7ae954761a4859
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-01-24 23:02:00 -0300

    N4A10 - Recuperação de Senha

---
+++ Nível-4/10-projeto-node/src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+import { v4 as uuidv4 } from 'uuid';
+
+import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokenRepository {
+  private usersTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuidv4(),
+      token: uuidv4(),
+      user_id,
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
