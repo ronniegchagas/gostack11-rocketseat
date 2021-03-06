290cf0ae1bbb33efdf03927b4ecbfa5502651b68
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-03-06 17:10:49 -0300

    N4A11 - Atualização do perfil

---
+++ Nível-4/11-continuando-back-end-do-app/src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,125 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfileService: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfileService = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  // Deve ser capaz de atualizar o perfil
+  it('should be able to update the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    const updateUser = await updateProfileService.execute({
+      // eslint-disable-next-line camelcase
+      user_id: user.id,
+      name: 'John Trê',
+      email: 'johntre@exemple.com',
+    });
+
+    expect(updateUser.name).toBe('John Trê');
+    expect(updateUser.email).toBe('johntre@exemple.com');
+  });
+
+  // Não deve ser capaz de mudar para outro e-mail existente
+  it('should not be able to change to another existing email', async () => {
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Test',
+      email: 'test@exemple.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        // eslint-disable-next-line camelcase
+        user_id: user.id,
+        name: 'John Doe',
+        email: 'johndoe@exemple.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  // Deve ser capaz de atualizar a senha
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      // eslint-disable-next-line camelcase
+      user_id: user.id,
+      name: 'John Trê',
+      email: 'johntre@exemple.com',
+      // eslint-disable-next-line camelcase
+      old_password: '123456',
+      password: '123123',
+    });
+
+    expect(updatedUser.password).toBe('123123');
+  });
+
+  // Não deve ser capaz de atualizar a senha sem a senha antiga
+  it('should not be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        // eslint-disable-next-line camelcase
+        user_id: user.id,
+        name: 'John Trê',
+        email: 'johntre@exemple.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  // Não deve ser capaz de atualizar a senha com a senha antiga errada
+  it('should not be able to update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        // eslint-disable-next-line camelcase
+        user_id: user.id,
+        name: 'John Trê',
+        email: 'johntre@exemple.com',
+        // eslint-disable-next-line camelcase
+        old_password: 'wrong-old-password',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
